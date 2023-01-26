250853a898acb177ee2b5f915897a27e635410c9
admin <nomizu7412@gmail.com>
2023-01-26 12:00:27 +0900

    My Django Girls app, first commit

---
+++ bbs/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Article(models.Model):
+    content = models.CharField(max_length=200)
+    user_name = models.CharField(max_length=1000, null = True)
+
+    def __str__(self):
+        return self.content
+
+
+
+
+   
